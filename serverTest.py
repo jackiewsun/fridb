5ba75d50f059399892f151478e13d1632d7fd2d3
Jackie Sun <sunjackiew@gmail.com>
2019-06-13 13:46:37 -0500

    Separated HTML

--- serverTest.py
+++ serverTest.py
@@ -2,11 +2,13 @@ from bottle import route, run, template, SimpleTemplate, static_file
 
 @route('/')
 def index():
-    with open('ind.tpl', 'r') as f:
+    with open('ind.template', 'r') as f:
         template = SimpleTemplate('\n'.join(f.readlines()))
     with open('allresults_new.json', 'r') as f:
         data = ''.join(f.readlines())
-    return template.render(data = data)
+    with open('home.html', 'r') as f:
+        text = ''.join(f.readlines())
+    return template.render(text = text, data = data)
 
 @route('/static/<filename>')
 def server_static(filename):
